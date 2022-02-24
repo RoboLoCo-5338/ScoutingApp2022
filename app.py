1a927852f94dd24857da622cbe806fc193d7da8c
Avyukth Nilajagi <73862038+AvyukthN@users.noreply.github.com>
2022-02-24 18:20:59 -0500

    updated code 2/24/2022

--- app.py
+++ app.py
@@ -16,14 +16,17 @@ e2 = Value('i', 0)
 
 app = Flask(__name__)
 
-def init_sheet():
+def init_sheet(sheet_num):
     scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
     creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 
     client = gspread.authorize(creds)
 
-    sheet = client.open("RoboLoco-Scouting-Test").sheet1
+    if sheet_num == 1:
+        sheet = client.open("RoboLoco-Scouting-Test").worksheet('teleop')
+    if sheet_num == 2:
+        sheet = client.open("RoboLoco-Scouting-Test").worksheet('autonomouse')
 
     return sheet
 
@@ -44,18 +47,14 @@ def home():
     if request.method == 'GET':
         return render_template('home.html')
 
-@app.route('/analytics', methods=['GET', 'POST'])
-def analytics():
-    x = [1, 2, 3]
-    y = [1, 2, 3]
-
+@app.route('/analytics_teleop', methods=['GET'])
+def analytics_display_teleop():
     scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-
     creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 
     client = gspread.authorize(creds)
 
-    sheet = client.open("RoboLoco-Scouting-Test").sheet1
+    sheet = client.open("RoboLoco-Scouting-Test").worksheet('teleop')
 
     data = sheet.get_all_records()
 
@@ -124,7 +123,7 @@ def analytics():
     sd_low_arr = [[str(key), sd_low[key]] for key in sd_low]    
     sd_climb_arr = [[str(key), sd_climb[key]] for key in sd_climb]    
 
-    return render_template('analytics.html', 
+    return render_template('analytics_display.html', 
                 len=len(final_data), 
                 final_data=final_data, 
                 radar_data=final_radar_data, 
@@ -138,6 +137,98 @@ def analytics():
                 low_avg_data=low_avg_data,
                 climb_avg_data=climb_avg_data
                 )
+
+@app.route('/analytics_auto', methods=['GET'])
+def analytics_display_auto():
+    scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open("RoboLoco-Scouting-Test").worksheet('autonomouse')
+
+    data = sheet.get_all_records()
+
+    graph_data = {}
+    radar_data = {}
+    high_goals = {}
+    low_goals = {}
+    climb_hash = {}
+    name_set = set([])
+
+    for record in data:
+        team_name = record['team_name'] 
+        if team_name not in name_set:
+            name_set.add(team_name)
+            graph_data.update({team_name: []})
+            radar_data.update({team_name: []})
+            high_goals.update({team_name: []})
+            low_goals.update({team_name: []})
+            climb_hash.update({team_name: []})
+
+        high_goal = int(record['high_goal'])
+        low_goal = int(record['low_goal'])
+        climb = int(record['climb'])
+
+        total_score = (2*high_goal) + low_goal + climb
+
+        graph_data[team_name].append(total_score)
+        high_goals[team_name].append(high_goal)
+        low_goals[team_name].append(low_goal)
+        climb_hash[team_name].append(climb)
+
+        if len(radar_data[team_name]) > 0:
+            radar_data[team_name][0][0] += high_goal
+            radar_data[team_name][0][1] += low_goal 
+            radar_data[team_name][0][2] += climb
+
+        else:
+            radar_data[team_name].append([high_goal, low_goal, climb])
+    
+    final_data = [[str(key), graph_data[key]] for key in graph_data]
+    final_radar_data = [[str(key), radar_data[key]] for key in radar_data]
+    high_goals_data = [[str(key), high_goals[key]] for key in high_goals]
+    low_goals_data = [[str(key), low_goals[key]] for key in low_goals]
+    climb_data = [[str(key), climb_hash[key]] for key in climb_hash]
+    high_avg_data = [[arr[0], np.average(np.array(arr[1]))] for arr in high_goals_data]
+    low_avg_data = [[arr[0], np.average(np.array(arr[1]))] for arr in low_goals_data]
+    climb_avg_data = [[arr[0], np.average(np.array(arr[1]))] for arr in climb_data]
+
+    sd_high = {}
+    sd_low = {}
+    sd_climb = {}
+    for team in high_goals_data:
+        vals = np.array(team[1])
+
+        sd_high.update({team[0]: np.std(vals)})
+    for team in low_goals_data:
+        vals = np.array(team[1])
+
+        sd_low.update({team[0]: np.std(vals)})
+    for team in climb_data:
+        vals = np.array(team[1])
+
+        sd_climb.update({team[0]: np.std(vals)})
+
+    sd_high_arr = [[str(key), sd_high[key]] for key in sd_high]    
+    sd_low_arr = [[str(key), sd_low[key]] for key in sd_low]    
+    sd_climb_arr = [[str(key), sd_climb[key]] for key in sd_climb]    
+
+    return render_template('analytics_display.html', 
+                len=len(final_data), 
+                final_data=final_data, 
+                radar_data=final_radar_data, 
+                high_goals_data=high_goals_data, 
+                low_goals_data=low_goals_data,
+                climb_data=climb_data,
+                sd_high=sd_high_arr,
+                sd_low=sd_low_arr,
+                sd_climb=sd_climb_arr,
+                high_avg_data=high_avg_data,
+                low_avg_data=low_avg_data,
+                climb_avg_data=climb_avg_data
+                )
+
 
 @app.route('/observing', methods=['GET', 'POST'])
 def observation():
@@ -164,7 +255,9 @@ def observation():
             'low_goal': request.json['low_goal'],
             'climb': request.json['climb_points'],
             'penalty': request.json['penalty'],
-            'notes': request.json['notes']
+            'notes': request.json['notes'],
+            'defense': request.json['defense'],
+            'auto': request.json['auto']
         }
 
         print(request.json)
@@ -181,7 +274,11 @@ def observation():
         # df.to_csv(f'./observations/{name}')
 
         # sheets API update calls
-        sheet = init_sheet()
+        s_num = 1
+        if data['auto']:
+            s_num = 2
+
+        sheet = init_sheet(sheet_num=s_num)
 
         col1 = sheet.col_values(1)
         team_index = len(col1) + 1
@@ -191,10 +288,11 @@ def observation():
         sheet.update_cell(team_index, 3, data['low_goal'])
         sheet.update_cell(team_index, 4, data['climb'])
         sheet.update_cell(team_index, 5, data['penalty'])
-        sheet.update_cell(team_index, 6, data['notes'])
-                
+        sheet.update_cell(team_index, 6, data['defense'])
+        sheet.update_cell(team_index, 7, data['notes'])
+            
         # sheet.update_cell(2, 1, name)
-        return render_template('home.html')
+        return redirect('home.html')
 
     if request.method == 'POST':
         if request.form.get('ball_shot'):
